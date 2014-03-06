e5870ef021209531c1bb762c35448428593eaeb6
Darkmacsek9 <inges.tamas@gmail.com>
2014-03-06 21:37:09 +0100

    Added Sandwich texture, disabled Attention until bugfix

--- waik_cheesemod/src/com/waik/cheesemod/CheeseBlock.java
+++ waik_cheesemod/src/com/waik/cheesemod/CheeseBlock.java
@@ -15,11 +15,11 @@ public class CheeseBlock extends Block {
 	}
 
 	@SubscribeEvent
-	public void onBlockClicked(PlayerInteractEvent event) {
-		System.out.println("cheese clicked");
-		EntityPlayer player = event.entityPlayer;
-		player.addChatMessage(new ChatComponentText("Attention! Someone somewhere clicked a cheese!!!"));
-	}
+	//public void onBlockClicked(PlayerInteractEvent event) {
+		//System.out.println("cheese clicked");
+		//EntityPlayer player = event.entityPlayer;
+		//player.addChatMessage(new ChatComponentText("Attention! Someone somewhere clicked a cheese!!!"));
+	//}
 
 	@Override
 	public void registerBlockIcons(IIconRegister reg) {
