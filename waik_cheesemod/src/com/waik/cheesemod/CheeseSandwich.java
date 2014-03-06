1eaf75ee69bccc5612daa03c9c57542cb3ce475a
Darkmacsek9 <inges.tamas@gmail.com>
2014-03-06 22:51:09 +0100

    Sandwich name fix

--- waik_cheesemod/src/com/waik/cheesemod/CheeseSandwich.java
+++ waik_cheesemod/src/com/waik/cheesemod/CheeseSandwich.java
@@ -5,7 +5,7 @@ import net.minecraft.item.ItemFood;
 public class CheeseSandwich extends ItemFood {
 	public CheeseSandwich(int i, int j, boolean b) {
 		super(j, b);
-		this.setUnlocalizedName("Cheese Sandwich");
+		this.setUnlocalizedName("cheeseSandwich");
 		this.setTextureName("cheesemod:cheese_sandwich");
 	}
 }
